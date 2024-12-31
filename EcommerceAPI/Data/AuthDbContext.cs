ca44a1ee7157d3d255f9df5cb2b83408e2c9c006
Theodor Suciu <theodorsuciu@yahoo.com>
2024-12-31 07:16:01 -0800

    Seeding Roles

--- EcommerceAPI/Data/AuthDbContext.cs
+++ EcommerceAPI/Data/AuthDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace EcommerceAPI.Data
@@ -7,7 +8,32 @@ namespace EcommerceAPI.Data
     {
         public AuthDbContext(DbContextOptions<AuthDbContext> options) : base(options)
         {
-            
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            var readerRoleId = "d0d9e53c-7070-4250-8809-206fb7a89182";
+            var writerRoleId = "a6d16b47-aab5-4c80-b5a6-ba261d34c392";
+            var roles = new List<IdentityRole>
+            {
+                new IdentityRole
+                {
+                    Id = readerRoleId,
+                    ConcurrencyStamp = readerRoleId,
+                    Name = "Reader",
+                    NormalizedName = "Reader".ToUpper()
+                },
+                new IdentityRole
+                {
+                    Id = writerRoleId,
+                    ConcurrencyStamp = writerRoleId,
+                    Name = "Writer",
+                    NormalizedName = "Writer".ToUpper()
+                }
+            };
+
+            builder.Entity<IdentityRole>().HasData(roles);
         }
     }
 }
